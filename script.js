fd39eec804f75027cc8d2eb8fb5fbeab68757daa
Jacqueline Winterbottom <jjwint90@gmail.com>
2018-01-31 16:56:51 -0500

    not working

--- script.js
+++ script.js
@@ -1,16 +1,22 @@
 (function(){
+
+	var startButton = document.getElementById("startButton");
 	var attackButton = document.getElementById("attackButton");
 	var userHealthBar = document.getElementById("userHealth");
-	var message El = document.getElementById("message");
+	var messageEl = document.getElementById("message");
 	var user = {
-		health: 40;
-	};
+		health: 40
+		}
+
+	startButton.onclick = function() {
+		var userName = prompt("What do you want to name your character?");
+	}
 
 	attackButton.onclick = function() {
 		user.health--;
 		updateDisplay();
 		updateMessage("Ouch!");
-	};
+	}
 
 	function updateDisplay() {
 		userHealthBar.value = user.health;
@@ -19,5 +25,7 @@
 	function updateMessage(newMessage) {
 		messageEl.innerText = newMessage;
 	}
+
+
 
 });
