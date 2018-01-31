fbcab231e989507f93ddb335fb65d233f26207af
Jacqueline Winterbottom <jjwint90@gmail.com>
2018-01-31 15:19:29 -0500

    initial commit

--- script.js
+++ script.js
@@ -1,12 +1,24 @@
 (function(){
 	var attackButton = document.getElementById("attackButton");
+	var userHealthBar = document.getElementById("userHealth");
+	var message El = document.getElementById("message");
+	var user = {
+		health: 40;
+	};
 
+	attackButton.onclick = function() {
+		user.health--;
+		updateDisplay();
+		updateMessage("Ouch!");
+	};
 
+	function updateDisplay() {
+		userHealthBar.value = user.health;
+	}
 
-
-
-
-
+	function updateMessage(newMessage) {
+		messageEl.innerText = newMessage;
+	}
 
 
 
