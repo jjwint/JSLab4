ca824af058769865cc3840cef8bf44bff5e22624
Jacqueline Winterbottom <jjwint90@gmail.com>
2018-02-01 09:05:27 -0500

    assignment due

--- script.js
+++ script.js
@@ -24,41 +24,88 @@
 
 	var startButton = document.getElementById("startButton");
 	var attackButton = document.getElementById("attackButton");
+	var healButton = document.getElementById("healButton");
+	var quitButton = document.getElementById("quitButton");
 	var userHealthBar = document.getElementById("characterHealth");
+	var healCountBar = document.getElementById("healCount");
+	var userWinsBar = document.getElementById("userWins");
 	var grantHealthBar = document.getElementById("grantHealth");
 	var messageEl = document.getElementById("message");
 	var mainGame = document.getElementById("mainGame");
+	var getUserName = document.getElementById("userName");
 
+	
+	
 	startButton.onclick = function() {
 		var userName = prompt("What do you want to name your character?");
 		startButton.classList.add("hideStart");
 		mainGame.classList.remove("hidden");
+
 	};
 
 	attackButton.onclick = function() {
 		character.health -= grant.generateAttackDamage();
 		grant.health -= character.generateAttackDamage();
 		updateDisplay();
-		updateMessage("Ouch!");
+		updateMessage("That's gotta hurt!");
+		if (grant.health <= 0) {
+			character.wins++;
+			userWinsBar.value++;
+			grant.health = 10;
+			updateDisplay();
+		}
 	};
 
 	healButton.onclick = function() {
 		if (character.healsRemaining > 0) {
 			character.heal();
+			updateMessage("You have been healed");
+			updateDisplay();
+		} else if (character.healsRemaining <= 0) {
+			updateMessage("You have no more heals remaining");
 		}
 	};
+
+	quitButton.onclick = function() {
+		mainGame.classList.add("hidden");
+	}
 
 	function updateDisplay() {
 		userHealthBar.value = character.health;
 		grantHealthBar.value = grant.health;
-		if (grant.health <= 0) {
-			grant.health = 10;
-		}
-		userHealthBar.value += character.heal();
+		healCountBar.value = character.healsRemaining;
+		userWinsBar.value = character.wins;
 	};
 
 	function updateMessage(newMessage) {
 		messageEl.innerText = newMessage;
 	};
 
+	if (character.health <= 0) {
+		updateMessage("The Almighty Grant is the winner!");
+	} else if (grant.health <= 0 && user.wins >= 5) {
+		updateMessage(userName + " is the winner!");
+	}
+	
+
+	// if (grant.health <= 0) {
+	// 	character.wins++;
+	// 	userWinsBar.value++;
+	// }
+
 })();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
